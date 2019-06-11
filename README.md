efd1eca2eaa0bed5c76b231d679eed3669f30e8a
Gavin Mendel-Gleason <gavin.mendel.gleason@gmail.com>
2019-06-11 10:47:54 +0100

    Update mic

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # typo
-Type and syntax checker reporting using metainterpretation.
+
+Typo is a type and syntax checker reporting tool using metainterpretation.
+
+## Metainterpertation critic 
+
+The metainterpertation critic (metainterpret/2) gives us information
+as to causes of failure for a restricted subset of prolog in which ';'
+is interpreted as exclusive disjunction.
+
+From the error tree, which is a kind of recording of the failed SLD
+tree, we can then determine a likely target for "most interesting
+failure", for instance by looking for those resolution paths of
+greatest depth.
+
+This library is useful for creating either an ad-hoc type-checker
+which verifies syntax of a term (well-formedness conditions etc.) or
+for creating a full fledged type checker.
+
+Author: Gavin Mendel-Gleason
+Copyright: Datachemist Ltd. 
+Licence: Apache License 2.0
--- mic.pl
+++ mic.pl
@@ -1,4 +1,7 @@
-:- module(mic,[metainterpret/2]).
+:- module(mic,[metainterpret/2,
+               isa_atom/1,
+               isa_integer/1,
+               isa_string/1]).
 
 /* <module> Metainterpertation critic 
  * 
@@ -31,26 +34,35 @@
 /**********************
  * Primitive type predicates 
  * 
- * 
+ * Gives the base types which can be checked by the checker, 
+ * prefixed by 'isa_' with the expectation that we will call this for 
+ * user-defined types.
  */
 isa_integer(X) :-
     integer(X).
 
-isa_list([]).
-isa_list([_|T]) :-
-    isa_list(T).
+isa_atom(X) :-
+    atom(X).
 
-isa_list([],integer).
-isa_list([H|T],integer) :-
-    isa_integer(H),
-    isa_list(T,integer).
-    
+isa_string(X) :-
+    string(X).
+
 /* 
  * metainterpret(Term,MaybeError:maybe(domain_error)) is det.
  * 
  * Metainterpret/2 does not fail, but rather keeps information about 
  * why it *would* fail under normal circumstances.
  */
+
+% This is intended for HM style type checking... needs testing
+% 
+% metainterpret(isa(X,T), ME) :-
+%     % if the type variable is still a variable, we
+%     % need to suspend the interpretation.
+%     var(T),
+%     !,
+%     when(ground(T),
+%          metainterpret(isa(X,T),ME)).
 metainterpret(true, none).
 metainterpret(\+ T, ME) :-
     (   \+ metainterpret(T,_)
@@ -70,13 +82,20 @@ metainterpret(isa_atom(X), ME) :-
     ;   ME = just(error_leaf(X, M)), 
         format(atom(M), '~q is not an atom', [X])
     ).
-metainterpret(isa(X,T), ME) :-
-    % if the type variable is still a variable, we
-    % need to suspend the interpretation.
-    var(T),
-    !,
-    when(ground(T),
-         metainterpret(isa(X,T),ME)).
+metainterpret(isa_string(X), ME) :-
+    (   string(X)
+    ->  ME = none
+    ;   ME = just(error_leaf(X, M)), 
+        format(atom(M), '~q is not a string', [X])
+    ).
+metainterpret(exclude(P,L,O), ME) :-
+    % We really need to lift to higher order predicate metainterperter
+    % calls here which requires some care.
+    (   call(exclude(P,L,O))
+    ->  ME = none
+    ;   ME = just(error_leaf(exclude(P,L,O), M)), 
+        format(atom(M), 'Exclusion with predicate ~q does not result in ~q on ~q', [P,O,L])
+    ).
 metainterpret(isa(X,T), ME) :-
     T =.. [F|Args],
     atom_concat('isa_',F,G),
@@ -123,6 +142,12 @@ metainterpret((TP1=TP2), ME) :-
         format(atom(Msg), 'Non unifiable term elements ~q and ~q', [TP1,TP2])
     ).
 metainterpret(P, ME) :-
+    functor(P,F,_),
+    \+ member(F,[isa,isa_integer,',',';', '=', true,':']),
+    !,
+    context_module(M),
+    metainterpret(M:P, ME).
+metainterpret(M:P, ME) :-
     functor(P,F,N),
     \+ member(F,[isa,isa_integer,',',';', '=', true]),
     !,
